dee6d64e200b7a3b6e5f790d1b821b5aae6e09b3
msoto6 <70609219+msoto6@users.noreply.github.com>
2021-04-13 16:45:42 -0500

    Update slidescript.js
    Code allows images to change in slideshow

--- assets/js/slidescript.js
+++ assets/js/slidescript.js
@@ -12,3 +12,21 @@ images[4] = "../assets/img/ImagePreview9.JPEG";
 images[5] = "../assets/img/ImagePreview10.JPEG";
 images[6] = "../assets/img/ImagePreview11.JPEG";
 images[7] = "../assets/img/ImagePreview12.JPEG";
+
+// Change Image
+function changeImg() {
+  document.getElementById("slide").src = images[i];
+
+  // Check If Index Is Under Max
+  if (i < images.length - 1) {
+    // Add 1 to Index
+    i++;
+  } else {
+    // Reset Back To O
+    i = 0;
+  }
+
+  // Run function every x seconds
+  setTimeout("changeImg()", time);
+}
+
