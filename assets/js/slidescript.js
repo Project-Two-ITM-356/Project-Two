bc38d062f134c60339ff8b3c03c423b63d3cb03b
msoto6 <70609219+msoto6@users.noreply.github.com>
2021-04-13 16:43:35 -0500

    Update slidescript.js
    Added Image List

--- assets/js/slidescript.js
+++ assets/js/slidescript.js
@@ -2,3 +2,13 @@
 var i = 0; // Start Point
 var images = []; // Images Array
 var time = 3000; // Time Between Switch
+
+// Image List
+images[0] = "../assets/img/ImagePreview5.JPEG";
+images[1] = "../assets/img/ImagePreview6.JPEG";
+images[2] = "../assets/img/ImagePreview7.JPEG";
+images[3] = "../assets/img/ImagePreview8.JPEG";
+images[4] = "../assets/img/ImagePreview9.JPEG";
+images[5] = "../assets/img/ImagePreview10.JPEG";
+images[6] = "../assets/img/ImagePreview11.JPEG";
+images[7] = "../assets/img/ImagePreview12.JPEG";
